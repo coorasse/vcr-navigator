b41a60178fa331be3b5aa5f4748144e411ff1084
GitHub Action <action@github.com>
2025-08-08 08:33:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/coorasse/vcrnavigator/MyBundle.kt
+++ src/main/kotlin/com/github/coorasse/vcrnavigator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.coorasse.vcrnavigator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/coorasse/vcrnavigator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/coorasse/vcrnavigator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.coorasse.vcrnavigator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.coorasse.vcrnavigator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
